3e4b32b431f85de8c04766d21108b0d19c5692b9
Jennifer Walker <dataqueen16@gmail.com>
2018-06-15 18:12:57 -0500

    added files from previous project

---
+++ src/add-profile.js
@@ -0,0 +1,53 @@
+    const addProfile = woman => {
+        const url = 'http://localhost:3000/women';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(woman),
+        };
+        return fetch(url, options)
+            .then(response => response.json());
+    };
+
+    const capitalize = (input) => {
+        input = input.toString();
+        var first = input[0].toUpperCase();
+        var rest = input.substring(1).toLowerCase();
+        return first + rest;
+    };
+
+    const capitalizeEachWord = (input) => {
+        var inputArray = input.split(" ");
+        var output = "";
+        inputArray.forEach(function (word) {
+            output += " " + capitalize(word);
+        });
+        return output;
+    };
+
+    $('#add-profile').on('click', function () {
+        console.log("Submitting");
+        addProfile({
+            name: capitalizeEachWord($('#woman-profile-name').val().toString()),
+            birthDate: ($('#woman-profile-birthdate').val()),
+            deathDate: ($('#woman-profile-death').val()),
+            living: ($('#woman-profile-living').val()),
+            bio: ($('#woman-profile-blurb').val()),
+        });
+        $('#woman-profile-name').val("");
+        $('#woman-profile-birthdate').val("");
+        $('#woman-profile-blurb').val("");
+        $('#hidden-input').addClass('invisible');
+        $('#woman-profile-death').val("");
+        $('#woman-profile-living').prop('selectedIndex', 0);
+    });
+
+    const livingValue = $('#woman-profile-living').val();
+
+    $('#woman-profile-living').change(function() {
+        console.log("changed");
+        $('#hidden-input').removeClass('invisible');
+    });
+
